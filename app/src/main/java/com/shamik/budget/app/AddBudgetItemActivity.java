64618e59f2b92e08307c3e8914be672ab2fd0397
Shamik Ganguly <shamgang@umich.edu>
2016-05-05 13:08:47 -0400

    budget item entry wireframe

--- app/src/main/java/com/shamik/budget/app/AddBudgetItemActivity.java
+++ app/src/main/java/com/shamik/budget/app/AddBudgetItemActivity.java
@@ -26,5 +26,28 @@ public class AddBudgetItemActivity extends BudgetAppActivity  {
 
         addDrawer();
 
+        // Check that the activity is using the layout version with
+        // the fragment_container FrameLayout
+        if (findViewById(R.id.budget_entry_container) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            // Create a new Fragment to be placed in the activity layout
+            AddBudgetItemFragment addBudgetItemFragment = new AddBudgetItemFragment();
+
+            // In case this activity was started with special instructions from an
+            // Intent, pass the Intent's extras to the fragment as arguments
+            addBudgetItemFragment.setArguments(getIntent().getExtras());
+
+            // Add the fragment to the 'fragment_container' FrameLayout
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.budget_entry_container, addBudgetItemFragment).commit();
+        }
+
     }
 }
---
+++ app/src/main/java/com/shamik/budget/app/AddBudgetItemFragment.java
@@ -0,0 +1,18 @@
+package com.shamik.budget.app;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Shamik on 5/5/2016.
+ */
+public class AddBudgetItemFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.add_budget_item_fragment, container, false);
+    }
+}
